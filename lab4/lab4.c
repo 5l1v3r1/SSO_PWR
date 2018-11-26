af052e0fe5d3f2a4dcaff35c369ec44589c3e792
205318 <s205318@zak.ict.pwr.wroc.pl>
2018-11-26 14:16:12 +0100

    , aby wyglądał tak, jak drugi.

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -2,14 +2,16 @@
 #include<stdio.h> 
 #include<stdlib.h> 
 #include<unistd.h> 
+
+#include <time.h>
   
-pthread_cond_t zm = PTHREAD_COND_INITIALIZER; 
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER; 
 
 pthread_mutex_t mtx;
 
-int acc1=4000;
-
+int acc1=20;
 int acc2=0;
+int acc3=0;
 
 /*
 
@@ -29,12 +31,11 @@ thread:
 */
 
 
-
 void wplata(int *a1,int howMuch){
 
 	int bmp=*a1;
 
-	usleep(1000);
+	usleep(rand()%1001);
 
 	*a1=bmp+howMuch;
 }
@@ -55,34 +56,149 @@ void przelew(int *a1,int *a2,int howMuch){
 }
 
 
-void *fthreadx(void *argp){
+void *fthread1(void *argp){
 
 	pthread_mutex_lock(&mtx);
 
-	for(int i=0;i<2000;++i){
+    // heading into monitor loop, we own the predicate mutex
+    while (1)
+    {
+        // check predicate
+        while (acc1<=0){
+            pthread_cond_wait(&cv, &mtx);
+			printf("Waiting for $ on account 1...\n");
+		}
 
-		przelew(&acc1,&acc2,1);
+        // some state that is part of the predicate to 
+        // inform us we're finished
+        if (0)
+            break;
 
-		printf("Acc1 is %d; acc2 is %d.\n",acc1,acc2);
-	}	
+        przelew(&acc1,&acc2,1);
 
-	pthread_mutex_unlock(&mtx);
+		printf("Acc1 is %d;\nAcc2 is %d;\nAcc3 is %d.\n\n",acc1,acc2,acc3);
 
-	return NULL;
+        // unlatch the mutex to do our predicate-independant work.
+        pthread_mutex_unlock(&mtx);
+
+		pthread_cond_signal(&cv);
+
+        usleep(rand()%140001);
+
+        // re-latch mutex prior to heading into wait
+        pthread_mutex_lock(&mtx);            
+    }
+
+    // we still own the mutex. remember to release it on exit
+    pthread_mutex_unlock(&mtx);
+
+    return NULL;
 }
+
+void *fthread2(void *argp){
+
+	pthread_mutex_lock(&mtx);
+
+    // heading into monitor loop, we own the predicate mutex
+    while (1)
+    {
+        // check predicate
+        while (acc2<=0){
+            pthread_cond_wait(&cv, &mtx);
+			printf("Waiting for $ on account 2...\n");
+		}
+
+        // some state that is part of the predicate to 
+        // inform us we're finished
+        if (0)
+            break;
+
+        przelew(&acc2,&acc3,1);
+
+		printf("Acc1 is %d;\nAcc2 is %d;\nAcc3 is %d.\n\n",acc1,acc2,acc3);
+
+        // unlatch the mutex to do our predicate-independant work.
+        pthread_mutex_unlock(&mtx);
+
+		pthread_cond_signal(&cv);
+
+        usleep(rand()%480001);
+
+        // re-latch mutex prior to heading into wait
+        pthread_mutex_lock(&mtx);            
+    }
+
+    // we still own the mutex. remember to release it on exit
+    pthread_mutex_unlock(&mtx);
+
+    return NULL;
+}
+
+void *fthread3(void *argp){
+
+	pthread_mutex_lock(&mtx);
+
+    // heading into monitor loop, we own the predicate mutex
+    while (1)
+    {
+        // check predicate
+        while (acc3<=0){
+			printf("Waiting for $ on account 3...\n");
+            pthread_cond_wait(&cv, &mtx);
+		}
+
+        // some state that is part of the predicate to 
+        // inform us we're finished
+        if (0)
+            break;
+
+        przelew(&acc3,&acc1,1);
+
+		printf("Acc1 is %d;\nAcc2 is %d;\nAcc3 is %d.\n\n",acc1,acc2,acc3);
+
+        // unlatch the mutex to do our predicate-independant work.
+        pthread_mutex_unlock(&mtx);
+
+		pthread_cond_signal(&cv);
+
+        usleep(rand()%420001);
+
+        // re-latch mutex prior to heading into wait
+        pthread_mutex_lock(&mtx);            
+    }
+
+    // we still own the mutex. remember to release it on exit
+    pthread_mutex_unlock(&mtx);
+
+    return NULL;
+}
+
+
 
 
 int main(){
 
 	pthread_t pt0;
 
+	srand(time(0));
+
+	int condition=0;
+
 	pthread_t pt1;
 
-	pthread_create(&pt1, NULL, fthreadx, NULL);
+	pthread_t pt2;
+
+	pthread_create(&pt0, NULL, fthread1, NULL);
+
+	pthread_create(&pt1, NULL, fthread2, NULL);
+
+	pthread_create(&pt2, NULL, fthread3, NULL);
 
 	pthread_join(pt0, NULL);
 
 	pthread_join(pt1, NULL);
+
+	pthread_join(pt2, NULL);
 
 
 	return 0;
